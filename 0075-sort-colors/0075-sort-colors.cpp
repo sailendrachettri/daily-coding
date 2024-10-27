f57742fc6dfc52d6c0e6f2e003c8cb262748ee1a
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-10-27 07:36:05 +0530

    Time: 0 ms (100%), Space: 10.3 MB (34.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int countOfZeros = 0;
+        int countOfOnes = 0;
+        int countOfTwos = 0;
+
+        int size = nums.size();
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0)
+                countOfZeros++;
+            else if(nums[i] == 1)
+                countOfOnes++;
+            else 
+                countOfTwos++;
+        }
+
+        for(int i = 0; i < size;){
+            while(countOfZeros > 0){
+                nums[i++] = 0;
+                countOfZeros--;
+            }
+            while(countOfOnes > 0){
+                nums[i++] = 1;
+                countOfOnes--;
+            }
+            while(countOfTwos > 0){
+                nums[i++] = 2;
+                countOfTwos--;
+            }
+        }
     }
 };
