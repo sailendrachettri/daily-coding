6e978e678689669ca15d32ea3063d0411f06c7d8
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-11-03 20:22:39 +0530

    Time: 7 ms (51.28%), Space: 16.6 MB (40.94%) - LeetHub

--- 0815-champagne-tower/0815-champagne-tower.cpp
+++ 0815-champagne-tower/0815-champagne-tower.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
-        vector<double> currRow(1, poured);
-
-        for(int i = 0; i <= query_row; i++){
-            vector<double> nextRow(i+2, 0);
-
-            for(int j = 0; j <= i; j++){
-                if(currRow[j] >= 1){
-                    nextRow[j] += (currRow[j]-1) / 2.0;
-                    nextRow[j+1] += (currRow[j]-1) / 2.0;
-                    currRow[j] = 1;
-                }
+        vector<vector<double>> dp(query_row+1,vector<double>(query_row+1,0));
+        
+        dp[0][0]=poured;
+        for(int i=1;i<=query_row;i++)
+        { 
+            dp[i][0]=dp[i][i]=(dp[i-1][0]-1)/2;    
+            for(int j=1;j<i;j++)
+            {
+                if(dp[i-1][j-1]>1 && dp[i-1][j]>1)
+                dp[i][j]=(dp[i-1][j-1]-1)/2+(dp[i-1][j]-1)/2;
+                else if(dp[i-1][j-1]>1)
+                dp[i][j]=(dp[i-1][j-1]-1)/2;
+                else
+                dp[i][j]=(dp[i-1][j]-1)/2;
             }
-
-            if(i != query_row)
-                currRow = nextRow;
         }
-
-        return currRow[query_glass];
+       if(dp[query_row][query_glass]>=1)
+       return 1;
+       else if(dp[query_row][query_glass]<=0)
+       return 0;
+       else
+       return dp[query_row][query_glass];
     }
 };
