e68fa0a956399fc0669fbf54c4320afbc4e2063e
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-11-28 20:24:14 +0530

    Time: 4 ms (5.42%), Space: 30.5 MB (5.34%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-//https://leetcode.com/problems/rotate-array/submissions/
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        k = k % n;
+        vector<int> rotated(n);
 
-        vector<int> temp(n);
-
-        for(int i = 0; i < n; i++)
-        {
-            temp[(i+k) % n] = nums[i]; // formula // if we calcualte mode of a numbr then we get a number between 0 to n-1
+        for(int i = 0; i < n; i++){
+            rotated[(i+k) % n] = nums[i];
         }
 
-        nums = temp; // copying the temp array to original array
+        for(int i = 0; i < n; i++){
+            nums[i] = rotated[i];
+        }
     }
 };
