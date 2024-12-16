152852b367cbd72b410de5b4b87a433befc46f69
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-12-16 22:43:56 +0530

    Time: 0 ms (100%), Space: 13.6 MB (21.62%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -8,14 +8,13 @@ public:
             int mid = start + (end - start) / 2;
 
             if(nums[mid] == target) return mid;
-            
+
             else if(nums[mid] < target)
                 start = mid + 1;
-
-            else if(nums[mid] > target)
+            else 
                 end = mid - 1;
         }
-        
-        return start;
+
+        return end+1;
     }
 };
