d4c2448b13ea0429bc5b7812428a7c46f7d195d0
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-12-18 19:47:35 +0530

    Time: 31 ms (7.53%), Space: 14.1 MB (17.75%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        vector<int> ans;
+        int dis;
+        int n = prices.size();
+
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){ 
+                dis = prices[j];
+                dis = min(dis, prices[j]);
+
+                if(prices[j] <= prices[i])
+                    break;
+            }
+
+            if(dis > prices[i]){
+                dis = 0;
+            }
+            cout << "dis " << dis << endl;
+
+            ans.push_back(prices[i]-dis);
+            dis = 0;
+        }
+
+        return ans;
+    }
+};
