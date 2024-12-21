0be6b2432dde2634da8878a8e605a309eebb263f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-12-21 19:45:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        long long res = 1;
+        
+        for(int i = 1; i <= n; i++){
+            if(i*i <= n){
+                res = i;
+            } else
+                break;
+        }
+        
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.floorSqrt(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
