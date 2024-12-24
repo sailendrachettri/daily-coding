346ecc2821f8bd48928bf975063cda0456715c65
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-12-24 20:24:46 +0530

    Time: 4 ms (26.01%), Space: 28.2 MB (34.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        int ans;
-        int maxi = 0;
-
-        for(int i = 0; i < nums.size(); i++){
-            freq[nums[i]]++;
-        }
-
-        for(auto item : freq){
-            if(item.second > maxi){
-                maxi = item.second;
-                ans = item.first;
-            }
-        }
-
-        return ans;
-
-
+        sort(nums.begin(), nums.end());
+        return nums[nums.size()/2];
     }
 };
