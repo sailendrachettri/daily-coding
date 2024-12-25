44005d1775bbf3a5e9c4c3e7a30bcb76a4db0118
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-12-25 22:30:08 +0530

    Time: 4 ms (34.05%), Space: 11 MB (8.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         string str = to_string(x);
-
         string str_copy = str;
-
         reverse(str.begin(), str.end());
 
-        return str == str_copy;
+        return str_copy == str;
     }
 };
