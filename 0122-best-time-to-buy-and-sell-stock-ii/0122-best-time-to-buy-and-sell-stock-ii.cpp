d45a237aa7b1f74f30c29be196423a65bd2e9bb0
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2024-12-26 20:09:41 +0530

    Time: 0 ms (100%), Space: 17.1 MB (38.11%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int profit = 0;
+    int maxProfit(vector<int>& p) {
+        int max_profit = 0;
+        int best_buy = p[0];
+        int n = p.size();
 
-        for(int i = 1; i < prices.size(); i++){
-            if(prices[i] > prices[i-1]){
-                profit += prices[i] - prices[i-1];
+        for(int i = 0; i < n-1; i++){
+            if((p[i+1]-best_buy) > 0){
+                max_profit += (p[i+1] - best_buy);
+                best_buy = p[i+1];
             }
+
+            best_buy = min(best_buy, p[i+1]);
         }
 
-        return profit;
+        return max_profit;
     }
 };
