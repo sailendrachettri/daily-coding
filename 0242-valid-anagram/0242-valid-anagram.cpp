87d923c11c9f4431181d667f20930d3bb4a5ee3f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-01-03 22:11:26 +0530

    Time: 7 ms (29.53%), Space: 10.5 MB (9.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        map<char, int> s_freq;
+        map<char, int> t_freq;
 
-        return s==t;
+        if(s.size() != t.size()) return false;
+
+        for(int i = 0; i < s.size(); i++){
+            s_freq[s[i]]++;
+        }
+        for(int i = 0; i < t.size(); i++){
+            t_freq[t[i]]++;
+        }
+
+        for(int i = 0; i < s_freq.size(); i++){
+            if(s_freq[i] != t_freq[i]) return false;
+        }
+
+        return true;
     }
 };
