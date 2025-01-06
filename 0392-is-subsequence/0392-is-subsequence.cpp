3709fd3bebf38d35a37a1b204544d55c6fdec8bd
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-01-06 20:55:01 +0530

    Time: 0 ms (100%), Space: 8.7 MB (23.94%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int sizeOfS = s.size();
-        int sizeOfT = t.size();
+        string ans = "";
+        int idx = 0;
 
-        if(sizeOfS > sizeOfT) return false;
-
-        int ch = s[0];
-        int ch_i = 0;
-
-        int i = 0;
-
-        while(i < sizeOfT){
-            if(ch == t[i]){
-                ch_i += 1;
-                ch = s[ch_i];
+        for(int i = 0; i < t.size(); i++){
+            if(t[i] == s[idx]){
+                ans += s[idx++];
             }
-            i++;
         }
 
-        cout << ch_i << " " << sizeOfS;
-
-        return ch_i == sizeOfS;
+        return s == ans;
     }
 };
