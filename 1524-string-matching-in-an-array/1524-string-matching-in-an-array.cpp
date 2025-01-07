9804223fd8bc3ab992a15560383003a0794a6e98
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-01-07 19:49:28 +0530

    Time: 0 ms (100%), Space: 12 MB (24.35%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        sort(words.begin(), words.end(), [](const string &s, const string&t){
+            return s.size() < t.size();
+        });
+
+        const int n = words.size();
+        vector<string> ans;
+        ans.reserve(n);
+
+        for(int i = 0; i < n-1; i++){
+            auto &s = words[i];
+
+            for(int j = i+1; j < n; j++){
+                auto &t = words[j];
+
+                if(t.find(s) != -1) {ans.push_back(s);break;}
+            }
+        }
+
+        return ans;
+    }
+};
