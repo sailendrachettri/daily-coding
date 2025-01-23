4ce71366fca6fd69f9847f76bd89b2b3e4c82b4d
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-01-23 20:14:39 +0530

    Time: 7 ms (7.25%), Space: 18.2 MB (5.33%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
+        
         int idx = 0;
+        string ans = "";
 
         for(int i = 0; i < strs[0].size(); i++){
-            char ch = strs[0][idx];
+            bool is_same = true;
 
             for(string st : strs){
-                if(st[idx] != ch) return ans;
-            }
 
-            ans += ch;
-            idx++;
+                if(st[idx] != strs[0][idx]){
+                    is_same = false;
+                    return ans;
+                }
+
+            }
+            if(is_same){
+                ans += strs[0][idx++];
+            }
         }
 
         return ans;
