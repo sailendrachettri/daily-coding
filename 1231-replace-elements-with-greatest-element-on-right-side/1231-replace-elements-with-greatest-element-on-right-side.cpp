f08ef24fee7c46f8c03c699ca7e15c27e25991b9
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-01-23 19:45:14 +0530

    Time: 0 ms (100%), Space: 72.8 MB (17.68%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n = arr.size();
-        int maxi;
-
-        for(int i = 0; i < n-1; i++){
-             maxi = INT_MIN;
-            for(int j = i+1; j < n; j++){
-                maxi = max(maxi, arr[j]);
-            }
-            arr[i] = maxi;
-        }
-        arr.pop_back();
         arr.push_back(-1);
+
+        for(int i = arr.size()-2; i >= 0; i--){
+            arr[i] = max(arr[i], arr[i+1]);
+        }
+
+        for(int i = 0; i < arr.size()-1; i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr.pop_back();
 
         return arr;
     }
