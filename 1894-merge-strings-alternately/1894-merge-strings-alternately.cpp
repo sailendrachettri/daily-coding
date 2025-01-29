72ac10aaa83870fc3a66e964ea4148f7a5431cd0
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-01-29 07:38:52 +0530

    Time: 0 ms (100%), Space: 8.5 MB (63.22%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans = "";
         int start = 0;
-        int end = word1.size() > word2.size() ? word2.size() : word1.size();
+        int end = word1.size() > word2.size() ? word2.size() - 1 : word1.size() - 1;
+        string ans = "";
 
-        while(start < end){
-            ans+= word1[start];
+        while(start <= end){
+            ans += word1[start];
             ans += word2[start];
             start++;
         }
+
         int start_copy = start;
 
-        while(start_copy < word1.size())
-            ans += word1[start_copy++];
-        while(start < word2.size())
-            ans += word2[start++];
+        while(start < word1.size()){
+            ans += word1[start++];
+        }
+
+        while(start_copy < word2.size()){
+            ans += word2[start_copy++];
+        }
 
         return ans;
     }
