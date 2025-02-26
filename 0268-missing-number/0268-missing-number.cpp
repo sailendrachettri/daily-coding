1c4056fd771d46ccb798be82000bfc308745d9e0
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-02-26 21:23:01 +0530

    Time: 18 ms (9.04%), Space: 21.7 MB (92.6%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,10 +3,15 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i <= nums.size(); i++){
+            if(i >= nums.size()) return i;
+            
             if(nums[i] != i) return i;
+
         }
 
-        return nums.size();
+        return -1;
+
+        
     }
 };
