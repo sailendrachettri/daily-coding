db4447d2b8db5002878ebf26b8168df163e5c080
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-03-08 19:37:04 +0530

    Time: 15 ms (11.57%), Space: 24.3 MB (5.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        map<int, int> freq;
 
-        for(int i = 0; i < nums.size(); i++){
-            freq[nums[i]]++;
-        }
+        for(int num : nums)
+            freq[num]++;
 
-        for(auto itr : freq){
-            cout << itr.first << " " << itr.second << endl;
-            if(itr.second == 1) return itr.first;
-        }
+        for(auto it : freq)
+            if(it.second == 1) return it.first;
 
         return -1;
     }
