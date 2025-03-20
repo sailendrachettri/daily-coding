28fae5f29c62a9c8170620ffa45eef942142ee9e
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-03-20 20:15:36 +0530

    Time: 18 ms (5.06%), Space: 22.1 MB (12.35%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<int> starts;
+        vector<int> ends;
+
+        for(int i = 0; i < intervals.size(); i++){
+            for(int j = 0; j < intervals[0].size()-1; j++){
+                starts.push_back(intervals[i][j]);
+                ends.push_back(intervals[i][j+1]);
+            }
+        }
+
+        starts.push_back(newInterval[0]);
+        ends.push_back(newInterval[1]);
+
+
+        sort(begin(starts), end(starts));
+        sort(begin(ends), end(ends));
+
+    
+        for(int i = 0; i < starts.size()-1;){
+            if(starts[i+1] <= ends[i]){
+                starts.erase(starts.begin()+(i+1));
+                ends.erase(ends.begin()+i);
+                if(i > 0)
+                    i--;
+            }else{
+                i++;
+            }
+        }
+
+        intervals.clear();
+
+        for(int val : starts){
+            cout << val << " ";
+        }
+
+        cout << endl;
+
+        for(int num : ends){
+            cout << num << " ";
+        }
+
+        for(int i = 0; i < starts.size(); i++){
+            intervals.push_back({starts[i], ends[i]});
+        }
+
+        return intervals;
+
+    }
+};
