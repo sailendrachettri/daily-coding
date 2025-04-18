813c05c778bf02fece57210ce8aa980a13a75a6e
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-04-18 21:26:15 +0530

    Time: 6 ms (34.47%), Space: 11 MB (24.61%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    string countAndSay(int n) {
-        
-        if(n == 1)
-            return "1";
-        
-        if(n == 2)
-            return "11";
-        
-        string s = "11";
-        
-        for(int i = 3; i <= n; i++)
-        {
-            string t = "";
-            s = s + '&';
-            
-            int c = 1;
-            
-            for(int j = 1; j < s.size(); j++)
-            {
-                if(s[j] != s[j-1])
-                {
-                    t = t + to_string(c);
-                    t = t + s[j-1];
-                    c = 1;
-                }
-                else
-                    c++;
-            }
-            
-            s = t;
+    using info = pair<int, char>; // (frequency, digit)
+    static string str(const vector<info>& fc) {
+        string ans;
+        ans.reserve(fc.size()<<1);
+        for (auto& [f, c] : fc) {
+            ans+= to_string(f);
+            ans.push_back(c);
         }
-        
-        return s;
+        return ans;
+    }
+    static string countAndSay(int n) {
+        if (n==1) return "1";
+        vector<info> fc={{1, '1'}};
+        string s1=str(fc);
+        for (int i=3; i<=n; i++) {
+            const int sz=s1.size();
+            char c=s1[0];
+            fc={{1, c}};
+            for (int i=1; i<sz; i++) {
+                if (s1[i]==c)
+                    fc.back().first++;
+                else {
+                    fc.emplace_back(1, s1[i]);
+                    c=s1[i];
+                }
+            }
+            s1=str(fc);
+        }
+        return s1;
     }
 };
