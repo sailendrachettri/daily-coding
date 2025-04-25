839020419764271d718ed399137a21482cd001df
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-04-25 20:19:45 +0530

    Time: 0 ms (100%), Space: 19.8 MB (85.01%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int m1 = 0;
-        int m2 = 0;
-        int ct1 = 0;
-        int ct2 = 0;
+        vector<int> ans;
+        int size = nums.size();
 
-        for(const int num : nums){
-            if(num == m1) ct1++;
-            else if(num == m2) ct2++;
-            else if(ct1 == 0){
-                m1 = num;
-                ct1++;
-            } else if(ct2 == 0){
-                m2 = num;
-                ct2++;
-            } else {
-                ct1--; ct2--;
-            }
-        }
-        ct1 = 0;
-        ct2 = 0;
+        map<int, int> freq;
 
-        for(const int num : nums){
-            if(num == m1) ct1++;
-            else if(num == m2) ct2++;
-        }
-
-        vector<int> res;
-
-        if(ct1 > nums.size()/3)
-            res.push_back(m1);
-        if(ct2 > nums.size()/3)
-            res.push_back(m2);
-
-        return res;
+        for(int num : nums)
+            freq[num]++;
+        
+        for(auto it : freq)
+            if(it.second > size/3)
+                ans.push_back(it.first);
+        
+        return ans;
     }
 };
