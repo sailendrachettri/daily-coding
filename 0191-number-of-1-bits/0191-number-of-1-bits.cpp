8458cd20f54ab7d7ef5599377ce2db0411f235e3
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-04-25 20:15:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (48.15%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        bitset<64> binary(n);
-        cout << binary << endl;
-        int count = 0;
-
-        cout << binary.size() << endl;
-        cout << binary[5] << endl;
+        bitset<32> binary(n);
+        int ans = 0;
 
         for(int i = 0; i < binary.size(); i++){
             if(binary[i] == 1)
-                count++;
+                ans++;
         }
 
-        return count;
+        return ans;
     }
 };
