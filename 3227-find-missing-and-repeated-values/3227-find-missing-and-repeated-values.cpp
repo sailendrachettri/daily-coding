6b812791b4fa3cf21884ebefbf417a2051bb2440
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-05-03 21:05:24 +0530

    Time: 32 ms (5.11%), Space: 32.1 MB (6.81%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        map<int, int> mp;
-        int rep, mis = INT_MIN;
+        int dub = NULL;
+        int mis = NULL;
 
-        for(int i = 1; i <= grid.size()*grid[0].size(); i++){
-            mp[i] = 0;
+        map<int, int> freq;
+
+        for(int i = 0; i <= grid.size()*grid.size(); i++){
+            freq[i] = 0;
         }
 
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                mp[grid[i][j]]++;
+                freq[grid[i][j]]++;
             }
         }
 
-        for(auto it : mp){
-            cout << it.first << " " << it.second << endl;
-            if(it.second >= 2)
-                rep = it.first;
+        for(auto it : freq){
             if(it.second == 0)
                 mis = it.first;
+            if(it.second > 1)
+                dub = it.first;
+
+            if(mis != NULL && dub != NULL)
+                return {dub, mis};
         }
 
-        // if(mis == INT_MIN) 
-        //     mis = grid.size()*grid[0].size();
-
-        return {rep, mis};
-
-
+        return {dub, mis};
     }
 };
