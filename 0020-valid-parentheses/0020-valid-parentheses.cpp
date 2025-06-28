6306b95277c82218b9063eaa75d15fa05d26941c
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-06-28 20:39:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (61.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,22 +4,22 @@ public:
         stack<char> st;
 
         for(char ch : s){
-            if(ch == '(' || ch == '{' || ch == '['){
+            if(ch == '(' || ch == '[' || ch == '{'){
                 st.push(ch);
-            } else{
-                if(st.empty()) return false;
+            }else{
+                if(st.empty())
+                    return false;
+                
                 char top = st.top();
                 st.pop();
 
-                if(ch == ')' && top != '('
-                || ch == '}' && top != '{'
-                || ch == ']' && top != '[' 
+                if((ch == ')' && top != '(') ||
+                   (ch == ']' && top != '[') ||
+                   (ch == '}' && top !=  '{')
                 ) return false;
             }
         }
 
-        if(st.empty()) return true;
-
-        return false;
+        return st.empty();
     }
 };
