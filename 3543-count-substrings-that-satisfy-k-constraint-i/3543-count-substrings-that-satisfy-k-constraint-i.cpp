c7c84716583e0691f40969e17f81149847448c1c
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-06-29 20:44:56 +0530

    Time: 1538 ms (5.01%), Space: 16.8 MB (5.97%) - LeetHub

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    bool check(string str, int k){
+        int count_0 = 0;
+        int count_1 = 0;
+
+        for(char ch : str){
+            if(ch == '0')
+                count_0++;
+            else if(ch == '1')
+                count_1++;
+        }
+
+        if(count_0 <= k || count_1 <= k)
+            return true;
+        
+        return false;
+    }
+    int countKConstraintSubstrings(string s, int k) {
+        int count = 0;
+        string str = "";
+        
+        for(int start = 0; start < s.size(); start++){
+            for(int end = 0; end < s.size(); end++){
+
+                for(int i = start; i <= end; i++){
+                    str += s[i];
+                }
+
+
+                if(str != ""){
+                    bool ans = check(str, k);
+                    cout << str  << " " << ans << endl;
+                    if(ans == true)
+                        count++;
+                }
+
+                str = "";
+                
+            }
+        }
+
+        return count;
+    }
+};
