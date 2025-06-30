db99d4bd32be61bdf36e1bda191b09c64a95efce
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-06-30 19:10:18 +0530

    Time: 4 ms (1.14%), Space: 16.2 MB (32.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        map<int, int> freq;
+
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == target)
-                return i;
+            freq[nums[i]] = i;
+        }
+        
+        auto it = freq.find(target);
+
+        if(it != freq.end()){
+            return it->second;
         }
 
         return -1;
