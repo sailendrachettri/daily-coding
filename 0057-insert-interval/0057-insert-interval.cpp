51e21df8f49694b33cd4f5852bdc846a5b801257
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-07-01 07:46:33 +0530

    Time: 19 ms (5.8%), Space: 22.2 MB (11.05%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,44 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<int> st;
-        vector<int> ed;
+        vector<int> starts;
+        vector<int> ends;
 
-        st.push_back(newInterval[0]);
-        ed.push_back(newInterval[1]);
-
-        for(auto it : intervals){
-            st.push_back(it[0]);
-            ed.push_back(it[1]);
+        for(int i = 0; i < intervals.size(); i++){
+            for(int j = 0; j < intervals[0].size()-1; j++){
+                starts.push_back(intervals[i][j]);
+                ends.push_back(intervals[i][j+1]);
+            }
         }
 
-        sort(begin(st), end(st));
-        sort(begin(ed), end(ed));
+        starts.push_back(newInterval[0]);
+        ends.push_back(newInterval[1]);
 
-        for(int val : st)
-            cout << val << " ";
-        cout << endl;
-        for(int val : ed)
-            cout << val << " ";
-        
-        intervals.clear();
+        sort(begin(starts), end(starts));
+        sort(begin(ends), end(ends));
 
-        for(int i = 0; i < st.size()-1;){
-            if(st[i+1] <= ed[i]){
-                st.erase(st.begin()+(i+1));
-                ed.erase(ed.begin()+i);
-                if(i > 0)
+        for(int i = 0; i < starts.size()-1; ){
+            if(starts[i+1] <= ends[i]){
+                starts.erase(starts.begin()+(i+1));
+                ends.erase(ends.begin()+i);
+
+                if(i > 0){
                     i--;
+                }
             }else{
                 i++;
             }
         }
 
-        for(int i = 0; i < st.size(); i++){
-            intervals.push_back({st[i], ed[i]});
+        for(int val : starts)
+            cout << val << " ";
+
+        cout << endl;
+        for(int val : ends)
+            cout << val << " ";
+
+        intervals.clear();
+
+        for(int i = 0; i < starts.size(); i++){
+            intervals.push_back({starts[i], ends[i]});
         }
 
         return intervals;
-        
     }
 };
