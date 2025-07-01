4b05bd067ce1d07dc0e1d451045dd830bda3f7e1
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-07-01 07:07:10 +0530

    Time: 7 ms (10.85%), Space: 95.9 MB (25.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
+        vector<int> m;
+
+        for(int val : nums1)
+            m.push_back(val);
+
+        for(int val : nums2)
+            m.push_back(val);
         
-        if(m > n)
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int l=0, r= m;
-        int total = m+n+1;
-        
-        while(l<=r)
-        {
-            int fir = l + (r-l)/2;
-            int sec = total/2 - fir;
-            
-            int l1=INT_MIN, l2=INT_MIN;
-            int r1=INT_MAX, r2=INT_MAX;
-            
-            if(fir > 0)
-                l1 = nums1[fir-1];
-            if(sec>0)
-                l2 = nums2[sec-1];
-            if((fir>=0) && (fir<m))
-                r1 = nums1[fir];
-            if((sec>=0) && (sec<n))
-                r2 = nums2[sec];
-            if(l1<=r2 && l2<=r1){
-                if((n+m)%2 == 0)
-                    return (max(l1, l2)+min(r1, r2))/2.0;
-                else
-                    return max(l1, l2);
-            }
-            else if(l1> r2)
-                r = fir-1;
-            else
-                l = fir+1;
-        }     
+        sort(begin(m), end(m));
+
+        // even
+        if(m.size()%2 == 0){
+            float val1 = m[m.size()/2];
+            float val2 = m[m.size()/2-1];
+
+            float ans = (val1+val2) / 2.0;
+
+            return ans;
+
+        }
+        // odd 
+        else if(m.size()%2 != 0){
+            float val1 = m[m.size()/2];
+
+            return (val1 * 1.0);
+        }
+
         return 0;
     }
 };
