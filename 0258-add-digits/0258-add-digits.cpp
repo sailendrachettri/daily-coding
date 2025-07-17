98f336f852e753d13717dd88546f8e7a54e8e517
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-07-17 20:11:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (9.52%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,29 +1,6 @@
 class Solution {
 public:
     int addDigits(int num) {
-        string str = to_string(num);
-        long long ans = 0;
-        long long temp = num;
-
-        if(str.size() <= 1)
-            return num;
-
-        while(str.size() > 1 && temp != 0){
-            ans += temp%10;
-            temp = temp/10;
-
-            if(temp == 0){
-                str = to_string(ans);
-                if(str.size() <= 1)
-                    return ans;
-                
-                temp = ans;
-                ans = 0;
-            }
-        }
-
-        return ans;
-
-        return 0;
+        return 1+(num-1)%9;
     }
 };
