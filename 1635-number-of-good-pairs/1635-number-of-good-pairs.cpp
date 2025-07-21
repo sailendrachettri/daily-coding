fd70e17a7c2d060ae0c85c1c927ad7d5c80d15f6
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-07-21 21:37:48 +0530

    Time: 0 ms (100%), Space: 9.8 MB (24.51%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -5,7 +5,7 @@ public:
 
         for(int i = 0; i < nums.size(); i++){
             for(int j = 0; j < nums.size(); j++){
-                if(i < j && nums[i] == nums[j])
+                if(nums[i] == nums[j] && i < j)
                     ans++;
             }
         }
