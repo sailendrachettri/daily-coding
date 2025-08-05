421ce0959f8a3e8b426ed959b69116cb36e8f774
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-05 19:37:45 +0530

    Time: 43 ms (86.4%), Space: 29.2 MB (39.54%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        set<vector<int>> st;
-
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size()-2; i++){
-            int start = i+1; int end = nums.size()-1;
+        vector<vector<int>>  ans;
 
-            while(start < end){
-                int sum = nums[i] + nums[start] + nums[end];
+        for(int i = 0; i < nums.size(); i++){
 
-                if(sum == 0){
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[start]);
-                    temp.push_back(nums[end]);
-                    st.insert(temp);
-                    temp.clear();
-                    start++; 
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+                int start = i+1;
+                int end = nums.size()-1;
+
+                while(start < end){
+                    int sum = nums[i] + nums[start] + nums[end];
+                    if(sum == 0){
+                        ans.push_back({nums[i], nums[start], nums[end]});
+                        start++; end--;
+
+                        while(start < end && nums[start] == nums[start-1]){
+                            start++;
+                        }
+                    }
+
+                    else if(sum < 0){
+                        start++;
+                    }else if(sum > 0){
+                        end--;
+                    }
+
+                        // start++; end--;
+                    
                 }
-
-                if(sum < 0){
-                    start++;
-                }else{
-                    end--;
-                }
-            }
-        }
-
-        for(vector<int> arr : st){
-            ans.push_back(arr);
         }
 
         return ans;
