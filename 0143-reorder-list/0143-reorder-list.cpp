1c95d970479a41347384e1a44a017cb2d56bd9b6
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-12 07:36:36 +0530

    Time: 21 ms (7.44%), Space: 22.8 MB (79.96%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -11,33 +11,40 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        vector<int> nums;
-        ListNode* temp = head;
 
-        while(temp){
-            nums.push_back(temp->val);
-            temp = temp->next;
+        if(!head) return;
+
+        ListNode*slow = head;
+        ListNode*fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int start = 0;
-        int end = nums.size()-1;
-        vector<int> ans;
+        ListNode*second = slow->next;
+        slow->next = nullptr;
+        ListNode* node = nullptr;
 
-        while(start<=end){
-            ans.push_back(nums[start++]);
-            if(start < end)
-                ans.push_back(nums[end--]);
+        while(second){
+            ListNode*temp = second->next;
+            second->next = node;
+            node = second;
+            second = temp;
         }
 
-        temp = head;
+        ListNode*first = head;
+        second = node;
 
-        reverse(ans.begin(), ans.end());
-
-        while(!ans.empty()){
-            temp->val = ans.back();
-            temp = temp->next;
-            ans.pop_back();
+        while(second){
+            ListNode*temp1 = first->next;
+            ListNode*temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
         }
+
 
     }
 };
