e05cd8bbaf844165167ca994d51263dce264cce6
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-19 07:20:10 +0530

    Time: 0 ms (100%), Space: 17 MB (31.19%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.cpp
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int size = nums.size();
-        
-        for(int i = 0; i < size; i++){
-            nums.push_back(nums[i]);
+        int idx = 0;
+
+        while(size--){
+            nums.push_back(nums[idx++]);
         }
 
         return nums;
