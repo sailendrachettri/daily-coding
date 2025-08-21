2d4deed72cd07301652a6d1990818f26bef68d7b
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-21 07:07:58 +0530

    Time: 170 ms (5.28%), Space: 23.3 MB (5.02%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
-    vector<string> stringMatching(vector<string>& words) {
-        vector<string> ans;
-
+    bool match(vector<string> words, int idx, string s){
         for(int i = 0; i < words.size(); i++){
-            for(int j = 0; j < words.size(); j++){
-                if(i != j){
-                    if(words[j].find(words[i]) != string::npos){
-                        ans.push_back(words[i]);
-                        break;
+            if(i != idx){
+                for(int k = 0; k < words[i].size(); k++){
+                    for(int len = 1; len <= words[i].size()-1; len++){
+                        if(s == words[i].substr(k, len))
+                            return true;
                     }
                 }
             }
+        }
+
+        return false;
+    }
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        int idx = 0;
+
+        for(string st : words){
+            bool is_sub = match(words, idx, st);
+
+            if(is_sub)
+                ans.push_back(st);
+
+            idx++;
         }
 
         return ans;
