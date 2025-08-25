1ef29518a96dec8ce075e5af740c278b664a4270
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-25 07:08:30 +0530

    Time: 210 ms (5.56%), Space: 31.9 MB (11.25%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int rep = INT_MAX, mis = INT_MAX;
-        map<int, int> freq;
+        unordered_map<int, int> mp;
+        int rep = INT_MIN, mis = INT_MIN;
 
-        for(int i  = 1; i <= grid.size()*grid[0].size(); i++){
-            freq[i] = 0;
+        for(int i = 1; i <= grid.size()*grid.size(); i++){
+            mp[i]= 0;
         }
 
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                freq[grid[i][j]]++;
+        for(vector<int> &arr : grid){
+            for(int val : arr){
+                mp[val]++;
             }
         }
 
-        for(auto it : freq){
+        for(auto it : mp){
+            cout << it.first <<  " " << it.second << endl;
             if(it.second == 0)
                 mis = it.first;
-            if(it.second > 1)
+            else if(it.second > 1)
                 rep = it.first;
             
-            if(mis != INT_MAX && rep != INT_MAX)
+            if(rep != INT_MIN && mis != INT_MIN)
                 return {rep, mis};
         }
 
-        return {-1, -1};
+        return {rep, mis};
+
     }
 };
