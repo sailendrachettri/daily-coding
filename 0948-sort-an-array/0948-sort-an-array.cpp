1c659c9d28de79249b9b296e5948b0f8030acc3a
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-28 07:12:32 +0530

    Time: 20 ms (89.5%), Space: 71.9 MB (69.66%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        map<int, int> freq;
-
-        for(int val : nums){
-            freq[val]++;
-        }
-        nums.clear();
-        for(auto it : freq){
-            for(int i = 0; i < it.second; i++){
-                nums.push_back(it.first);
-            }
-        }
+        stable_sort(nums.begin(), nums.end());
 
         return nums;
     }
