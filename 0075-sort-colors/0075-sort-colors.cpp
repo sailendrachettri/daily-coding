fb191a44706314f82dc67c1e994c2703790aff96
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-08-28 07:29:27 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         map<int, int> freq;
-
-        for(int  num : nums)
-            freq[num]++;
-
+        for(int val : nums){
+            freq[val]++;
+        }
         nums.clear();
-
         for(auto it : freq){
-            int size = it.second;
-
-            while(size--){
+            for(int i = 0; i < it.second; i++){
                 nums.push_back(it.first);
             }
         }
