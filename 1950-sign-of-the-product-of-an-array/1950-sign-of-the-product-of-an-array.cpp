65eb4e2ff8edf18020035074c2c83fc00052f702
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-04 08:09:11 +0530

    Time: 0 ms (100%), Space: 13.8 MB (11.46%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    int mod = 100000000+7;
-
     int arraySign(vector<int>& nums) {
-        long long prod = 1;
+        int prod = 1;
 
-        for(int val : nums){
-            prod = prod * val;
-            prod = prod%mod;
+        for(int &val : nums){
+            if(val == 0) return 0;
+            else{
+                if(val > 0){
+                    val = 1;
+                    prod = val * prod;
+                }
+                else{
+                    val = -1;
+                    prod = prod * val;
+                }
+            }
         }
 
-        if(prod == 0) return 0;
-        else if(prod > 0) return 1;
-        return -1;
+        return prod;
     }
 };
