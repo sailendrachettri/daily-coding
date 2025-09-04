a5dd535a95e90ca735be7584ce9972796467d10f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-04 08:12:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (55.2%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     string clearDigits(string s) {
         string ans = "";
-
-        for(char ch : s){
-            if(ch >= '0' && ch <= '9')
-                ans.pop_back();
-            else
-                ans.push_back(ch);
+        for(char val : s){
+            if(val >= '0' && val <= '9'){
+                if(ans.size() != 0)
+                    ans.pop_back();
+            }else{
+                ans.push_back(val);
+            }
         }
 
         return ans;
