89a15c1bd2eb2849d5437cd21116fbc773af0809
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-09 07:15:34 +0530

    Time: 3 ms (21.94%), Space: 8.7 MB (83.54%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int ans = 0;
-        int left_sum = 0;
-        int right_sum = 0;
-
+        int score = 0;
         for(int i = 0; i < s.size()-1; i++){
+            int left = 0, right = 0;
+
             for(int j = 0; j <= i; j++){
                 if(s[j] == '0')
-                    left_sum++;
+                    left++;
             }
 
             for(int j = i+1; j < s.size(); j++){
                 if(s[j] == '1')
-                    right_sum++;
+                    right++;
             }
 
-            ans = max(ans, (left_sum+right_sum));
-            left_sum = 0;
-            right_sum = 0;
+            score = max(score, (left + right));
+
+            left = 0, right = 0;
         }
 
-        return ans;
+        return score;
     }
 };
