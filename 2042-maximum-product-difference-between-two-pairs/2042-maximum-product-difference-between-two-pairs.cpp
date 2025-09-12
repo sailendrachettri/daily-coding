4cad108cc3dfef13a010c578eda602efbd00c747
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-12 07:06:51 +0530

    Time: 21 ms (58.01%), Space: 24.1 MB (70.7%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-
-        int size = nums.size();
         sort(nums.begin(), nums.end());
 
-        int firstNum = nums[size-1];
-        int secondNum = nums[size-2];
-
-        int thirdNum = nums[1];
-        int fourthNum = nums[0];
-
-        int diff = (firstNum * secondNum) - (thirdNum*fourthNum);
-
-        return diff;
+        return ((nums[nums.size()-1]*nums[nums.size()-2]) - (nums[0]*nums[1]));
     }
 };
