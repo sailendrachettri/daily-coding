3412759ae5f5c481ecddd9d1a9cf863e861908b2
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-15 07:09:42 +0530

    Time: 23 ms (34.63%), Space: 28.4 MB (63.24%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -3,11 +3,12 @@ public:
     string removeStars(string s) {
         string ans = "";
 
-        for(int i = 0; i < s.size(); i++){
-            if(ans.size() > 0 && s[i] == '*')
+        for(char ch : s){
+            if(ch != '*'){
+                ans += ch;
+            }else if(ch == '*' && ans.size()){
                 ans.pop_back();
-            else
-                ans.push_back(s[i]);
+            }
         }
 
         return ans;
