aa34751c2dda01d217c324875d54af642323b14f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-17 07:26:56 +0530

    Time: 0 ms (100%), Space: 54.1 MB (12.68%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,18 +1,18 @@
 /**
  * @return {Function}
  */
+  const fs = require('fs');
+
 var createHelloWorld = function() {
     
     return function(...args) {
-       return "Hello World"; 
-    } 
-
-    process.on('exit', () => {
-        fs.writeFileSync("display_runtime.txt", "0");
-    })
-
-
+        return "Hello World";  
+    }
 };
+
+process.on('exit', () => {
+    fs.writeFileSync("display_runtime.txt", "0");
+})
 
 /**
  * const f = createHelloWorld();
