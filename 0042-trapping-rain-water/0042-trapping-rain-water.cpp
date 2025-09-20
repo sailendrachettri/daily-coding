c01e1e2cac75c16e074eb70583cddd3fa4d63cfb
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-20 07:44:04 +0530

    Time: 0 ms (100%), Space: 27.8 MB (5.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,37 +4,26 @@ public:
         vector<int> maxLeft(height.size(), 0);
         vector<int> maxRight(height.size(), 0);
         vector<int> mini(height.size(), 0);
-        vector<int> ans(height.size(), 0);
+        int size = height.size();
 
         int count = 0;
-        
 
-        for(int i = 1; i < height.size(); i++){
+        for(int i = 1; i < size; i++)
             maxLeft[i] = (max(height[i-1], maxLeft[i-1]));
-        }
 
-        for(int i = height.size()-2; i >= 0; i--){
+        for(int i = size-2; i >= 0; i--)
             maxRight[i] = (max(height[i+1], maxRight[i+1]));
-        }
 
-        for(int i = 0; i < height.size(); i++){
+        for(int i = 0; i < size; i++)
             mini[i] = min(maxLeft[i], maxRight[i]);
-        }
 
-        for(int i = 0; i < height.size(); i++){
+        for(int i = 0; i < size; i++){
             int val = mini[i]- height[i];
 
             if(val < 0)
                 val = 0;
-            ans[i] = val;
+            count += val;
         }
-
-        for(int i = 0; i < height.size(); i++){
-            count += ans[i];
-        }
-
-        for(int val : mini)
-            cout << val << " ";
 
         return count;
     }
