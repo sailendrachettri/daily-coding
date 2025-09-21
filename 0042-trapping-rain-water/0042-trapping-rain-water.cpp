c11e8aa79e7ad58cc917453ae002f7bb83bbeff0
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-21 07:06:04 +0530

    Time: 179 ms (5.19%), Space: 27.8 MB (5.12%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        vector<int> maxLeft(height.size(), 0);
-        vector<int> maxRight(height.size(), 0);
-        vector<int> mini(height.size(), 0);
+        vector<int>max_left(height.size(), 0);
+        vector<int>max_right(height.size(), 0);
+        vector<int>mini(height.size(), 0);
         int size = height.size();
-
         int count = 0;
 
-        for(int i = 1; i < size; i++)
-            maxLeft[i] = (max(height[i-1], maxLeft[i-1]));
+        for(int i = 1; i < size; i++){
+            max_left[i] = max(max_left[i-1], height[i-1]);
+        }
 
-        for(int i = size-2; i >= 0; i--)
-            maxRight[i] = (max(height[i+1], maxRight[i+1]));
-
-        for(int i = 0; i < size; i++)
-            mini[i] = min(maxLeft[i], maxRight[i]);
+        for(int i = size-2; i >= 0; i--){
+            max_right[i] = max(max_right[i+1], height[i+1]);
+        }
 
         for(int i = 0; i < size; i++){
-            int val = mini[i]- height[i];
+            mini[i] = min(max_left[i], max_right[i]);
+        }
 
-            if(val < 0)
-                val = 0;
-            count += val;
+        for(int val : mini)
+            cout << val << " ";
+
+        for(int i = 0; i < size; i++){
+            int val = mini[i] - height[i];
+            if(val > 0) count += val;
         }
 
         return count;
