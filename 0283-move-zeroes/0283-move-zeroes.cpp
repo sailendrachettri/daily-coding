a428c1893d9548cb67df14e4c1ffb52ee7bb308b
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-09-30 15:13:12 +0530

    Time: 19 ms (8.28%), Space: 23.8 MB (55.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int count = 0;
+        int idx = 0;
 
-        for(int i = 0; i < nums.size(); ){
-            if(nums[i]== 0){
-                nums.erase(nums.begin()+i);
-                if(i > 0){
-                    i--;
-                }
-                count++;
-            }else{
-                i++;
-            }
-        }
-
-        // add 0
-        for(int i = 1; i <= count; i++){
-            nums.push_back(0);
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[idx] == 0){
+                nums.erase(nums.begin()+idx);
+                nums.push_back(0);
+            }else
+                idx++;
         }
     }
 };
