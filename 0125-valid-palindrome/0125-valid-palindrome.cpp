a237e366a0a107d0298cd61f5b85a0b149d79b45
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-06 07:31:29 +0530

    Time: 0 ms (100%), Space: 9.9 MB (57.2%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,19 +4,24 @@ public:
         int start = 0;
         int end = s.size()-1;
 
+       
+        
         while(start < end){
-            while(start < end && !(isalnum(s[start]))){
+             while(start < s.size() && !isalnum(s[start]) )
                 start++;
-            }
-            while(end >= start && !(isalnum(s[end]))){
+            while( end >= 0 && !isalnum(s[end]))
                 end--;
-            }
 
             if(start > end) return true;
 
-            if(tolower(s[start]) != tolower(s[end]))
+            char ch1 = tolower(s[start]);
+            char ch2 = tolower(s[end]);
+
+
+            if(ch1 != ch2){
                 return false;
-            
+            }
+
             start++; end--;
         }
 
