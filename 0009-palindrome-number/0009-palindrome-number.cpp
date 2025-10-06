8b6e0d83531415325079dfe95e9f427bd12ece51
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-06 22:03:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (40.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x < 0) return false;
-        
         long long rev = 0;
-        int x2 = x;
+        int x_copy = x;
 
-        while(x2){
-            rev = rev * 10 + (x2%10);
-            x2 = x2 / 10;
+        while(x_copy){
+            rev = rev * 10 + (x_copy%10);
+            x_copy /= 10;
         }
 
-        return x == rev;
+        return rev == x;
     }
 };
