cdf489a2f72c325ee52cd991e291a55a1eefae55
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-07 19:42:22 +0530

    Time: 0 ms (100%), Space: 12.3 MB (64.1%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int hIndex(vector<int>& c) {
         sort(c.begin(), c.end(), greater<int>());
-
         int ans = 0;
-        for(int i = 0; i < c.size(); i++){
-            if(c[i] > ans)
+
+        for(int val : c){
+            if(val > ans)
                 ans++;
             else
                 return ans;
