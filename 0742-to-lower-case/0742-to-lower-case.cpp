c9bd4509e1d0cee340fda31fb1ad49560184d157
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-07 20:38:41 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.75%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.cpp
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(char &ch : s){
-            if(ch >= 'A' && ch <= 'Z' && (int)ch <= 97)
-                ch = (int)ch + 32;
+            if(ch >= 'A' && ch <= 'Z')
+                ch += 32;
         }
 
         return s;
