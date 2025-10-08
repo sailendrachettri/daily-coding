7319607f5434acf239b97d9bba07a6e9cbbb756c
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-08 07:34:03 +0530

    Time: 0 ms (100%), Space: 9 MB (32.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,26 +3,24 @@ public:
     bool isValid(string s) {
         stack<char> st;
 
-        for(int ch : s){
+        for(char ch : s){
             if(ch == '(' || ch == '[' || ch == '{')
                 st.push(ch);
             else{
-                if(st.empty())
-                    return false;
-                
+                if(st.empty()) return false;
+
                 char top = st.top();
                 st.pop();
 
                 if(top == '(' && ch != ')' ||
-                    top == '[' && ch != ']' ||
-                    top == '{' && ch != '}'
+                   top == '[' && ch != ']' ||
+                   top == '{' && ch != '}'
                 ) return false;
             }
         }
 
-        if(st.empty())
-            return true;
-        
+        if(st.empty()) return true;
+
         return false;
     }
 };
