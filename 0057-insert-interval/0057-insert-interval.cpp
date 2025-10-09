7428afe84d6df83c0c28818cf4f655c0e3cba37f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-09 07:25:43 +0530

    Time: 14 ms (5.75%), Space: 22.8 MB (5.5%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<int> starts;
-        vector<int> ends;
+        vector<int> starts, ends;
 
-        for(int i = 0; i < intervals.size(); i++){
-            for(int j = 0; j < intervals[0].size()-1; j++){
-                starts.push_back(intervals[i][j]);
-                ends.push_back(intervals[i][j+1]);
-            }
+        for(vector<int> arr : intervals){
+            starts.push_back(arr[0]);
+            ends.push_back(arr[1]);
         }
 
         starts.push_back(newInterval[0]);
         ends.push_back(newInterval[1]);
 
-        sort(begin(starts), end(starts));
-        sort(begin(ends), end(ends));
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
 
-        for(int i = 0; i < starts.size()-1; ){
-            if(starts[i+1] <= ends[i]){
-                starts.erase(starts.begin()+(i+1));
-                ends.erase(ends.begin()+i);
-
-                if(i > 0){
-                    i--;
-                }
-            }else{
+        for(int i = 1; i < starts.size();){
+            if(starts[i] <= ends[i-1]){
+                starts.erase(starts.begin()+i);
+                ends.erase(ends.begin()+(i-1));
+            }else
                 i++;
-            }
         }
-
-        for(int val : starts)
-            cout << val << " ";
-
-        cout << endl;
-        for(int val : ends)
-            cout << val << " ";
 
         intervals.clear();
 
