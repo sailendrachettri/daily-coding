dc8055ba2b4556e2488d3e81e6d18c137ead0ad4
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-09 07:17:32 +0530

    Time: 15 ms (8.04%), Space: 25.5 MB (5.86%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<int> start;
-        vector<int> ends;
+        vector<int> starts, ends;
 
-        for(int i = 0; i < intervals.size(); i++){
-            for(int j = 0; j < intervals[0].size()-1; j++){
-                start.push_back(intervals[i][j]);
-                ends.push_back(intervals[i][j+1]);
-            }
+        for(vector<int> arr : intervals){
+            starts.push_back(arr[0]);
+            ends.push_back(arr[1]);
+        }
+
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+
+        for(int i = 1; i < starts.size(); ){
+            if(starts[i] <= ends[i-1]){
+                starts.erase(starts.begin()+i);
+                ends.erase(ends.begin()+(i-1));
+            }else
+                i++;
         }
 
         intervals.clear();
 
-        sort(begin(start), end(start));
-        sort(ends.begin(), ends.end());
-
-       for(int i = 0; i < start.size()-1;){
-            if(start[i+1] <= ends[i]){
-                start.erase(start.begin()+i+1);
-                ends.erase(ends.begin()+i);
-
-                if(i > 0){
-                    i--;
-                }
-            }else{
-                    i++;
-                }
-       }
-
-       for(int i = 0; i < start.size(); i++){
-            intervals.push_back({start[i], ends[i]});
-       }
+        for(int i = 0; i < starts.size(); i++){
+            intervals.push_back({starts[i], ends[i]});
+        }
 
         return intervals;
     }
