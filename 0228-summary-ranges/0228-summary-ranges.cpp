ed73fd6ce62b5387d0a9711a283901ad61aa45bc
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-13 22:00:56 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41.06%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    const long long mod = 10000000+7;
-
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
+        int left = 0;
 
-        if(nums.size() == 1)
-            return {to_string(nums[0])};
+        if(nums.size() == 0) return ans;
 
-        if(nums.size() == 0) return {};
-            
-
-        for(long long i = 0, j = 0; i < nums.size();){
-            while(j < nums.size()-1 && (nums[j]%mod-nums[j+1]%mod) == -1){
-                j++;
+        for(int i = 1; i <= nums.size(); i++){
+            if(i == nums.size() || nums[i-1] != (nums[i]-1)){
+                if(left == i-1)
+                    ans.push_back(to_string(nums[left]));
+                else{
+                    string st = "";
+                    st += to_string(nums[left]);
+                    st += "->";
+                    st += to_string(nums[i-1]);
+                    ans.push_back(st);
+                }
+                left = i;
             }
-
-            if(nums[i] == nums[j])                
-                ans.push_back(to_string(nums[j]));
-
-            else{
-                ans.push_back(
-                    to_string(nums[i]) + "->" + to_string(nums[j])
-                );
-            }
-
-            j++;
-            i = j;
-            
         }
 
         return ans;
