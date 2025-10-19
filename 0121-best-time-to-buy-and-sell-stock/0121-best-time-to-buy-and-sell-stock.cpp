f690a24bbe0be48cd4dcb7ef6c64fcfeabd80550
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-19 07:05:46 +0530

    Time: 4 ms (17.91%), Space: 97.5 MB (7.09%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int best_buy = prices[0];
 
         for(int i = 1; i < prices.size(); i++){
-            max_profit = max(max_profit, (prices[i]-best_buy));
+            max_profit = max(max_profit, (prices[i] - best_buy));
             best_buy = min(best_buy, prices[i]);
         }
 
