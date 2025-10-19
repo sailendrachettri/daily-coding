20870a0b1033dcfdc3fb3c8745bb883c23354cfd
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-19 19:59:26 +0530

    Time: 2 ms (40.01%), Space: 10.8 MB (6.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int start = 0;
-        int end = s.size()-1;
+        string ans = "";
 
-       
-        
-        while(start < end){
-             while(start < s.size() && !isalnum(s[start]) )
-                start++;
-            while( end >= 0 && !isalnum(s[end]))
-                end--;
-
-            if(start > end) return true;
-
-            char ch1 = tolower(s[start]);
-            char ch2 = tolower(s[end]);
-
-
-            if(ch1 != ch2){
-                return false;
-            }
-
-            start++; end--;
+        for(int i = 0; i < s.size(); i++){
+            if(isalnum(s[i]))
+                ans += tolower(s[i]);
         }
 
-        return true;
+        string rev = ans;
+        reverse(rev.begin(), rev.end());
+
+        return ans == rev;
     }
 };
