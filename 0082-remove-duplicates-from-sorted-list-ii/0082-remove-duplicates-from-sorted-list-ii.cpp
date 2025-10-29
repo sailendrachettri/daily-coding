3a3cdac2c96de63ae0e736268c2ce832e3db3e92
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-10-29 07:35:28 +0530

    Time: 4 ms (5.53%), Space: 16.7 MB (5.58%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -13,36 +13,28 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         map<int, int> mp;
         vector<int> arr;
-
+        
         ListNode* temp = head;
         ListNode* dummy = new ListNode(0);
 
-        while(temp != NULL){
+        while(temp){
             mp[temp->val]++;
             temp = temp->next;
         }
 
         for(auto it : mp){
-            if(it.second == 1)
+            if(it.second == 1){
                 arr.push_back(it.first);
+            }
         }
-
-        for(int val : arr)
-            cout << val << " ";
-        
 
         temp = dummy;
 
-        reverse(arr.begin(), arr.end());
-
-        while(arr.size() > 0){
-            temp->next = new ListNode(arr.back());
+        for(int i = 0; i < arr.size(); i++){
+            temp->next = new ListNode(arr[i]);
             temp = temp->next;
-            arr.pop_back();
         }
 
         return dummy->next;
-
-
     }
 };
