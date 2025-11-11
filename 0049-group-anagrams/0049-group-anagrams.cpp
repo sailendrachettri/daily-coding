38b5122b4bf004140af5d1ddc15aade10a725666
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-11 18:18:28 +0530

    Time: 15 ms (71.63%), Space: 26.1 MB (39.34%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -5,15 +5,14 @@ public:
         vector<vector<string>> ans;
 
         for(string s : strs){
-            string st = s;
+            string copy = s;
+            sort(copy.begin(), copy.end());
 
-            sort(st.begin(), st.end());
-
-            mp[st].push_back(s);
+            mp[copy].push_back(s);
         }
 
-        for(auto it : mp){
-            ans.push_back(it.second);
+        for(auto m : mp){
+            ans.push_back(m.second);
         }
 
         return ans;
