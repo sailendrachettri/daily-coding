92bd08fcecc7bcb153da3e778629a78b27376091
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-11 18:12:34 +0530

    Time: 3 ms (68.76%), Space: 14.9 MB (19.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,10 +4,10 @@ public:
         unordered_map<int, int> mp;
 
         for(int i = 0; i < nums.size(); i++){
-            auto it = mp.find(target-(nums[i]));
+            auto it = mp.find(target - nums[i]);
 
             if(it != mp.end()){
-                return {it->second, i};
+                if(it->first + nums[i] == target) return {it->second, i};
             }
 
             mp[nums[i]] = i;
