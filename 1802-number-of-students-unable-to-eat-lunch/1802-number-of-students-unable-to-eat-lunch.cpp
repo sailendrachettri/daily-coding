b9478810a9bec3f6e39c26cebefe221566305d13
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-17 21:20:13 +0530

    Time: 0 ms (100%), Space: 11.8 MB (69.84%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int arr[2] = {0};
+        int count_1 = 0, count_0 = 0;
 
         for(int val : students){
-            arr[val]++;
+            if(val == 0)
+                count_0++;
+            else count_1++;
         }
 
+
         for(int i = 0; i < sandwiches.size(); i++){
-            int val = sandwiches[i];
+            if(sandwiches[i] == 0){
+                if(count_0 == 0) return sandwiches.size()-i;
+                count_0--;
+            }else{
+                if(count_1 == 0) return sandwiches.size()-i;
+                count_1--;
+            }
 
-            if(arr[val] == 0) return sandwiches.size()-i;
-
-            arr[val]--;
         }
 
         return 0;
