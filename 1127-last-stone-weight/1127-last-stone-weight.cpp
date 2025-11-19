988c4eb00795274c25899cbab3fa4897a4ab146e
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-19 07:15:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.23%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size() > 1){
+            sort(stones.begin(), stones.end());
+            int val1 = stones.back();
+            stones.pop_back();
+            int val2 = stones.back();
+            stones.pop_back();
+
+            int res = abs(val1-val2);
+
+            if(res > 0)
+                stones.push_back(res);
+        }
+
+        if(stones.empty()) return 0;
+
+        return stones[0];
+    }
+};
