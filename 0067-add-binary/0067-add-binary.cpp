4cb013c45365f5cb706dc2195c18c572e565c1d6
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-20 20:44:47 +0530

    Time: 66 ms (5.12%), Space: 9.1 MB (33.39%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,20 +1,48 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string res = "";
+        int a_size = a.size();
+        int b_size = b.size();
+        int diff = abs(a_size-b_size);
         int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        string ans = "";
 
-        while(i >= 0 || j >= 0 || carry){
-            if(i >= 0) carry += a[i--] - '0';
-            if(j >= 0) carry += b[j--] - '0';
-
-            res += carry % 2 + '0';
-            carry /= 2;
+        if(a_size < b_size){
+            for(int i = 0; i < diff; i++){
+                a.insert(a.begin(), '0');
+            }
+        }else if(b_size < a_size){
+            for(int i = 0; i < diff; i++){
+                b.insert(b.begin(), '0');
+            }
         }
 
-        reverse(begin(res), end(res));
-        return res;
+        for(char ch : a){
+            cout << ch << " ";
+        }cout << endl;
+        
+        for(char ch : b){
+            cout << ch << " ";
+        }
+        cout << endl;
+
+        for(int i = a.size()-1; i >= 0; i--){
+            int sum = (a[i]-'0') + (b[i]-'0') + carry;
+            ans += ((sum%2)+'0');
+            cout << ans << endl;
+            carry = sum / 2;
+        }
+
+        cout << "\ncarry " << carry << endl;
+
+        if(carry == 1){
+            ans.push_back('1');
+        }
+
+        reverse(ans.begin(), ans.end());
+
+
+        return ans;
+        
     }
 };
