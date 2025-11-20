62031dd99feeae1c967d6eba21c3239636371043
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-20 12:01:00 +0530

    Time: 15 ms (3.49%), Space: 12.9 MB (24.98%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.cpp
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    bool isnumber(string s){
-        try{
-            int val = stoi(s);
-            return true;
-            throw s;
-        }catch(...){
-            return false;
-        }
-    }
     int calPoints(vector<string>& operations) {
         stack<int> st;
-        int ans = 0;
+        int sum = 0;
 
         for(string s : operations){
-            if(isnumber(s)){
-                st.push(stoi(s));
-            }else if(s == "+"){
-                int val = st.top();
-                st.pop();
-                int tot = st.top()+val;
-                st.push(val);
-                st.push(tot);
-            }else if(s == "D"){
-                st.push(st.top()*2);
-            }else{
-                st.pop();
+            if(!st.empty()){
+                cout << st.top() << " ";
             }
+            if(s == "+"){
+                int val1 = st.top();
+                st.pop();
+                int val2 = st.top();
+                st.pop();
+                st.push(val2);
+                st.push(val1);
+                st.push(val1+val2);
+
+            }else if(s == "D") st.push(st.top()*2);
+            else if(s == "C") st.pop();
+            else st.push(stoi(s));
+            
         }
 
         while(!st.empty()){
-            ans += st.top();
+            sum += st.top();
             st.pop();
         }
 
-        return ans;
+        return sum;
     }
 };
