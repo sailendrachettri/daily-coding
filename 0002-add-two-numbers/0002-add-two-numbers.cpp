098726e63ba32b5a43f0d6ea9241d4988b1743e9
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-21 07:11:34 +0530

    Time: 0 ms (100%), Space: 77.2 MB (13.46%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -12,38 +12,10 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* dummy = new ListNode(0);
-        ListNode* temp1 = l1;
-        ListNode* temp2 = l2;
-        ListNode* temp3 = dummy;
-        int diff = 0;
-        int len1 = 0, len2 = 0;
+        ListNode* temp1;
+        ListNode* temp2;
+        ListNode* temp3;
         int carry = 0;
-
-        while(temp1->next != NULL){
-            len1++;
-            temp1 = temp1->next;
-        }
-        while(temp2->next != NULL){
-            len2++;
-            temp2 = temp2->next;
-        }
-
-        cout << len1 << " " << len2;
-
-        diff = abs(len1-len2);
-
-        if(len1 < len2){
-            while(diff--){
-                temp1->next = new ListNode(0);
-                temp1 = temp1->next;
-            }
-
-        }else if(len2 < len1){
-            while(diff--){
-                temp2->next = new ListNode(0);
-                temp2 = temp2->next;
-            }
-        }
 
         temp1 = l1; temp2 = l2;
         temp3 = dummy;
@@ -53,6 +25,21 @@ public:
             temp3->next = new ListNode(sum%10);
             carry = sum / 10;
             temp1 = temp1->next;
+            temp2 = temp2->next;
+            temp3 = temp3->next;
+        }
+
+        while(temp1){
+            int sum = temp1->val + carry;
+            temp3->next = new ListNode(sum%10);
+            carry = sum / 10;
+            temp1 = temp1->next;
+            temp3 = temp3->next;
+        }
+        while(temp2){
+            int sum = temp2->val + carry;
+            temp3->next = new ListNode(sum%10);
+            carry = sum / 10;
             temp2 = temp2->next;
             temp3 = temp3->next;
         }
