0cde8623f1cc4067cf09a418e1498dad67e59a22
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-21 06:54:21 +0530

    Time: 8 ms (7.61%), Space: 77.1 MB (74.5%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,55 +11,54 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int> a1, a2, ans;
-        ListNode* temp = l1;
-        int diff, size1, size2, carry;
         ListNode* dummy = new ListNode(0);
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        ListNode* temp3 = dummy;
+        int diff = 0;
+        int len1 = 0, len2 = 0;
+        int carry = 0;
 
-        while(temp){
-            a1.push_back(temp->val);
-            temp = temp->next;
+        while(temp1->next != NULL){
+            len1++;
+            temp1 = temp1->next;
+        }
+        while(temp2->next != NULL){
+            len2++;
+            temp2 = temp2->next;
         }
 
-        temp = l2;
+        cout << len1 << " " << len2;
 
-        while(temp){
-            a2.push_back(temp->val);
-            temp = temp->next;
-        }
+        diff = abs(len1-len2);
 
-        size1 = a1.size();
-        size2 = a2.size();
-        diff = abs(size1-size2);
-
-        if(a1.size() > a2.size()){
-            for(int i = 0; i < diff; i++){
-                a2.push_back(0);
+        if(len1 < len2){
+            while(diff--){
+                temp1->next = new ListNode(0);
+                temp1 = temp1->next;
             }
-        }else{
-            for(int i = 0; i < diff; i++){
-                a1.push_back(0);
+
+        }else if(len2 < len1){
+            while(diff--){
+                temp2->next = new ListNode(0);
+                temp2 = temp2->next;
             }
         }
 
-        carry = 0;
-        for(int i = 0; i < a1.size(); i++){
-            int sum = a1[i]+a2[i]+carry;
-            ans.push_back(sum%10);
-            sum = sum / 10;
-            carry = sum;
-            
+        temp1 = l1; temp2 = l2;
+        temp3 = dummy;
+
+        while(temp1 && temp2){
+            int sum = temp1->val + temp2->val + carry;
+            temp3->next = new ListNode(sum%10);
+            carry = sum / 10;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+            temp3 = temp3->next;
         }
 
-        temp = dummy;
-
-        for(int i = 0; i < ans.size(); i++){
-            temp->next = new ListNode(ans[i]);
-            temp = temp->next;
-        }
-
-        if(carry != 0){
-            temp->next = new ListNode(carry);
+        if(carry){
+            temp3->next = new ListNode(1);
         }
 
         return dummy->next;
