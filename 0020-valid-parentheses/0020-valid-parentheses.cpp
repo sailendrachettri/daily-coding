caba75cbeec9942984132dd9dc2018272a0a3809
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-11-22 07:17:34 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -4,17 +4,17 @@ public:
         stack<char> st;
 
         for(char ch : s){
-            if(ch == '(' || ch == '[' || ch == '{')
+            if(ch == '(' || ch == '[' || ch == '{'){
                 st.push(ch);
-            else{
+            }else{
                 if(st.empty()) return false;
 
                 char top = st.top();
                 st.pop();
 
-                if(top == '(' && ch != ')'  ||
-                   top == '[' && ch != ']' || 
-                   top == '{' && ch != '}' 
+                if(top == '(' && ch != ')' ||
+                    top == '{' && ch != '}' ||
+                    top == '[' && ch != ']'
                 ) return false;
             }
         }
