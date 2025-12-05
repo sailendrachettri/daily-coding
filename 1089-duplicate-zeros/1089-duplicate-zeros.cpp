30959a71800e460bb5e88a052b72f293e5a70338
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-05 06:42:02 +0530

    Time: 0 ms (100%), Space: 14.3 MB (17.91%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] != 0){
+                ans.push_back(arr[i]);
+            }
+            else {
+               ans.push_back(0);
+               ans.push_back(0);
+            }
+        }
+
+        while(ans.size() > n) ans.pop_back();
+        arr.clear();
+        arr = ans;
+    }
+};
