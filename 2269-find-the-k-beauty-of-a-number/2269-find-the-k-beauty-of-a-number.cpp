ffeccd89d0db18b35825f24d3a80ed925d8eefd8
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-05 20:12:26 +0530

    Time: 0 ms (100%), Space: 8.2 MB (40.65%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     int divisorSubstrings(int num, int k) {
         string st = to_string(num);
+        int size = st.size();
         int ans = 0;
 
-        for(int start = 0; start < st.size(); start++){
-            for(int end = start; end < st.size(); end++){
-                int val = 0;
-                int len = 0;
-                for(int i = start; i <= end; i++){
-                    val = val * 10 + (st[i]-'0');
-                    len++;
-                }
+        for(int i = 0; i < st.size(); i++){
+            int val = 0;
+            int t = k;
+            int idx = i;
+            int len = 0;
 
-                
-                if(val != 0 && len == k && num%val == 0){
-                    ans++;
-                }
+            while(idx < size && t--){
+                len++;
+                val = val * 10 + (st[idx]-'0');
+                idx++;
+            }
+
+            if(len == k && val != 0 && num%val == 0){
+                ans++;
             }
         }
 
