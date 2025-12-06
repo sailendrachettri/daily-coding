e19bc262f17adcfb2cd9b35f84e34c8c26ffaaa9
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-06 07:30:47 +0530

    Time: 0 ms (100%), Space: 14.9 MB (82.44%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        map<pair<int, int>, int> mp;
         int sum = 0;
 
         for(int i = 0; i < mat.size(); i++){
             for(int j = 0; j < mat[0].size(); j++){
-                if(!(mp.find({i, j}) != mp.end())){
-                    mp[{i, j}]++;
-
-                    if(i == j || (i+j == mat.size()-1))
-                        sum += mat[i][j];
+               
+                if(i == j || (i+j == mat.size()-1)){
+                    sum += mat[i][j];
                 }
-            }
-        }
 
-        for(auto it : mp){
-            cout << it.first.first << " " << it.second;
+            }
         }
 
         return sum;
