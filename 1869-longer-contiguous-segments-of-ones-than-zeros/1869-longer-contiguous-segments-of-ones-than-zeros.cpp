a78ea1f0479c45517afc072be6ff9275755a3b70
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2025-12-10 07:14:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (40.08%) - LeetHub

---
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int ones = 0, zeros = 0;
+        int count0 = 0, count1 = 0;
+
+        for(char val : s){
+            if(val == '0'){
+                count0++;
+                ones = max(ones, count1);
+                count1 = 0;
+            }else{
+                zeros = max(count0, zeros);
+                count1++;
+
+                count0 = 0;
+            }
+        }
+
+        ones = max(ones, count1);
+        zeros = max(count0, zeros);
+
+        return ones > zeros;
+    }
+};
