2411aad8307d3e1163cee42b1c35b1116552d6a2
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-10 07:05:55 +0530

    Time: 706 ms (13.37%), Space: 295.5 MB (31.96%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -3,36 +3,28 @@ public:
     void merge(vector<int> &arr, int low, int mid, int high){
         vector<int> temp;
         int left = low;
-        int right = mid+1;
+        int right = mid + 1;
 
         while(left <= mid && right <= high){
-            if(arr[left] <= arr[right]){
-                temp.push_back(arr[left]);
-                left++;
-            }else {
-                temp.push_back(arr[right]);
-                right++;
-            }
+            if(arr[left] <= arr[right])
+                temp.push_back(arr[left++]);
+
+            else 
+                temp.push_back(arr[right++]);
         }
 
-        while(left <= mid){
-            temp.push_back(arr[left]);
-            left++;
-        }
+        while(left <= mid)
+            temp.push_back(arr[left++]);
 
-        while(right <= high){
-            temp.push_back(arr[right]);
-            right++;
-        }
-
-        
+        while(right <= high)
+            temp.push_back(arr[right++]);
 
         for(int i = low; i <= high; i++){
             arr[i] = temp[i-low];
         }
     }
     void mergeSort(vector<int> &arr, int low, int high){
-        if(low == high) return;
+        if(low == high) return ;
 
         int mid = (low + high) / 2;
 
@@ -42,6 +34,7 @@ public:
     }
     vector<int> sortArray(vector<int>& nums) {
         mergeSort(nums, 0, nums.size()-1);
+
         return nums;
     }
 };
