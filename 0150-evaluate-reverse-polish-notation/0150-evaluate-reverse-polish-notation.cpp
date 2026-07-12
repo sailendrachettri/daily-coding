480fcb315fca5e5a12e1b08536091b3ce60a0073
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-12 19:11:59 +0530

    Time: 0 ms (100%), Space: 17 MB (87.89%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,55 +1,25 @@
 class Solution {
 public:
-    bool isInt(string s){
-        try{
-            stoi(s);
-        }catch(exception e){
-            return false;
-        }
-
-        return true;
-    }
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
-        int sum = 0;
 
         for(string &s : tokens){
-            if(!st.empty())
-                cout << st.top() << " ";
-            if(isInt(s)){
+            if(s == "+" || s == "-" || s == "*" || s == "/"){
+                int val1 = st.top();
+                st.pop();
+                
+                int val2 = st.top();
+                st.pop();
+
+                if(s == "+") st.push(val2+val1);
+                else if(s == "-") st.push(val2 - val1);
+                else if(s == "*") st.push(val2*val1);
+                else if(s == "/") st.push(val2/val1);
+            }else {
                 st.push(stoi(s));
-            }else if(s == "+"){
-                int val1 = st.top();
-                st.pop();
-                int val2 = st.top();
-                st.pop();
-                st.push(val1+val2);
-            }else if(s == "-"){
-                int val1 = st.top();
-                st.pop();
-                int val2 = st.top();
-                st.pop();
-                st.push(val2 - val1);
-            }else if(s == "*"){
-                int val1 = st.top();
-                st.pop();
-                int val2 = st.top();
-                st.pop();
-                st.push(val1 * val2);
-            }else if(s == "/"){
-                int val1 = st.top();
-                st.pop();
-                int val2 = st.top();
-                st.pop();
-                st.push(val2 / val1);
             }
         }
 
-        while(!st.empty()){
-            sum += st.top();
-            st.pop();
-        }
-
-        return sum;
+        return st.top();
     }
 };
