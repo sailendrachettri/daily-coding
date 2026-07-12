f57bd2736464ea4e702163c0aa703d546e965bc3
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-12 16:08:01 +0530

    Time: 617 ms (33.35%), Space: 295.6 MB (22.34%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -8,21 +8,22 @@ public:
         while(left <= mid && right <= high){
             if(arr[left] <= arr[right])
                 temp.push_back(arr[left++]);
-            else 
-                temp.push_back(arr[right++]);
+            else temp.push_back(arr[right++]);
         }
 
-        while(left <= mid)
+        while(left <= mid){
             temp.push_back(arr[left++]);
+        }
 
-        while(right <= high)
+        while(right <= high){
             temp.push_back(arr[right++]);
+        }
 
         for(int i = low; i <= high; i++){
             arr[i] = temp[i-low];
         }
     }
-    void mergeSort(vector<int> & arr, int low, int high){
+    void mergeSort(vector<int> &arr, int low, int high){
         if(low == high) return;
 
         int mid = (low + high) / 2;
