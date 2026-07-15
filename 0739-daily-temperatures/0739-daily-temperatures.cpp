23d83452940f79cd4c565a717a9b39691c0c84bc
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-15 21:25:26 +0530

    Time: 16 ms (80.46%), Space: 103 MB (59.11%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,18 +3,17 @@ public:
     vector<int> dailyTemperatures(vector<int>& t) {
         stack<int> st;
         vector<int> ans(t.size(), 0);
-        int right = t.size();
 
-        while(right--){
-            while(!st.empty() && t[right] >= t[st.top()]){
+        for(int i =  t.size()-1; i >= 0; i--){
+            while(!st.empty() && t[i] >= t[st.top()]){
                 st.pop();
             }
 
             if(!st.empty()){
-                ans[right] = st.top() - right;
+                ans[i] = st.top()-i;
             }
 
-            st.push(right);
+            st.push(i);
         }
 
         return ans;
