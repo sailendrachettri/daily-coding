a9c4d69df0c5bc00ecefeaf9aaddb15e93494818
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-22 07:28:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.32%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,21 +4,16 @@ public:
         int start = 0;
         int end = x;
 
-        int ans = INT_MIN;
-
-        if(x == 1) return x;
+        int ans = 0;
 
         while(start <= end){
             long long mid = midpoint(start, end);
 
-            cout << "mid: " << mid << endl << "sq: " << (mid*mid*1LL) << endl;
-
             if(mid*mid*1LL <= x){
-                if(mid > ans) ans = mid;
+                ans = mid;
                 start = mid+1;
             }
-            else if(mid*mid*1LL > x) end = mid-1;
-            else start = mid+1;
+           else end = mid-1;
         }
 
         return ans;
