b2c617f50a73033cd377c9feddc038ad3a4db663
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-22 07:25:56 +0530

    Time: 183 ms (6.56%), Space: 8.6 MB (14.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x, 0.5);
+        int start = 0;
+        int end = x;
+
+        int ans = INT_MIN;
+
+        if(x == 1) return x;
+
+        while(start <= end){
+            long long mid = midpoint(start, end);
+
+            cout << "mid: " << mid << endl << "sq: " << (mid*mid*1LL) << endl;
+
+            if(mid*mid*1LL <= x){
+                if(mid > ans) ans = mid;
+                start = mid+1;
+            }
+            else if(mid*mid*1LL > x) end = mid-1;
+            else start = mid+1;
+        }
+
+        return ans;
     }
 };
