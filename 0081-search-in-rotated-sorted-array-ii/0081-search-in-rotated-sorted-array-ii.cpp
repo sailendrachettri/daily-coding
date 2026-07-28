e5221e5ca382c2f93e319698088ffc52a11327f8
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-07-28 07:53:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int val : nums)
-            if(val == target)
-                return true;
-        
+        for(int &val : nums){
+            if(val == target) return true;
+        }
+
         return false;
     }
 };
