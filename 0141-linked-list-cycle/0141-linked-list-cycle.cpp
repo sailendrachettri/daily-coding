86d5c954b86b2db86b7d2091eea3872b4b80098f
Sailendra Chettri <49643664+sailendrachettri@users.noreply.github.com>
2026-08-05 20:01:10 +0530

    Time: 13 ms (19.73%), Space: 11.9 MB (54.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -14,11 +14,11 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next != NULL){
+        while(fast !=  NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
-
             if(slow == fast) return true;
+
         }
 
         return false;
